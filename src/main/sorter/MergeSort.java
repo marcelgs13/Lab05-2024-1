c6657e3258dd25965a0da3f08f0fbf693666be96
marcelgs13 <119629090+marcelgs13@users.noreply.github.com>
2024-08-15 18:26:05 +0000

    commit_1

--- src/main/sorter/MergeSort.java
+++ src/main/sorter/MergeSort.java
@@ -9,6 +9,23 @@ public class MergeSort implements Sorter {
     }
 
     public int[] merge(int[] left, int[] right) {
-        return null;
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < left.length) {
+            result[k++] = right[j++];
+        }
+        return result;
     }
 }
--- src/main/sorter/QuickSorter.java
+++ src/main/sorter/QuickSorter.java
@@ -7,7 +7,21 @@ public class QuickSorter implements Sorter {
     }
 
     public int partition(int[] values, int low, int high) {
-        return 0;
-    }
-    
+        int pivo = values[low];
+        int i = low;
+        int temp = 0;
+        for(int j = low + 1; j <= high; j++){
+            if (values[j] <= pivo){
+                i+=1;
+                temp = values[i];
+                values[i]= values[j];
+                values[j] = temp;
+            }
+        }
+        temp = values[i];
+        values[i]= values[low];
+        values[low]= temp;
+        return i;
+
+    }    
 }
